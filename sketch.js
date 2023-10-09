3077a774f70c677119d4946c2f95e0a9699feffe
Difei (Amber) Gao <dg3294@nyu.edu>
2023-10-09 17:42:06 -0400

    Start Exploring

--- sketch.js
+++ sketch.js
@@ -1,5 +1,20 @@
+let table;
+
+function preload(){
+  table = loadTable('HighestHolywoodGrossingMovies.csv', 'csv', 'header');
+}
+
+
 function setup() {
+  // print(table.getRowCount() + ' total rows in table');
+  // print(table.getColumnCount() + ' total columns in table');
+  // print(table.getColumn('name'));
   createCanvas(windowWidth, windowHeight);
+
+
+  console.log(table);
+
+
 }
 
 function draw() {
